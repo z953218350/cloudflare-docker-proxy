15de312e2577fa53c98c7e4960f6958d7e9fa4c8
zengxianghui <953218350@qq.com>
2024-07-07 03:19:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.502205.xyz": dockerHub,
+  "quay.502205.xyz": "https://quay.io",
+  "gcr.502205.xyz": "https://gcr.io",
+  "k8s-gcr.502205.xyz": "https://k8s.gcr.io",
+  "k8s.502205.xyz": "https://registry.k8s.io",
+  "ghcr.502205.xyz": "https://ghcr.io",
+  "cloudsmith.502205.xyz": "https://docker.cloudsmith.io",
+  "ecr.502205.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.502205.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
